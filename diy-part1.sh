232442689c479e28f5db927f3fd8cea5ef38c728
kinglove1990 <295856977@qq.com>
2024-11-08 08:30:50 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,6 @@
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+# 添加files
+git clone https://github.com/tsg2k2/files.git
